8b61387ac3d39da931400ba3ab0ef5bdc02ef9b0
farukemrekaraca <farukemrekaraca@gmail.com>
2025-12-28 16:27:30 +0300

    clarification

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # must be changed in prod
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
